cc2ffc34eb6bb3fb040448e08e22ee9676fe94f2
AnveshaSharan <anvesha0707@gmail.com>
2026-08-24 01:48:24 +0530

    day3

---
+++ day3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {2, 7, 11, 15};
+    int target = 9;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                printf("[%d, %d]", i, j);
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
